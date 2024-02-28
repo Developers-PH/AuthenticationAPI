611786197d6c942b57926a598a30295e0df9e8d3
aysonsteven <aysonsteven@gmail.com>
2024-02-28 17:03:51 +0800

    initial commit

---
+++ src/main/java/com/devph/authenticationapi/authenciationapi/AuthenciationApiApplication.java
@@ -0,0 +1,13 @@
+package com.devph.authenticationapi.authenciationapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenciationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenciationApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devph/authenticationapi/authenciationapi/AuthenciationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devph.authenticationapi.authenciationapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenciationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
